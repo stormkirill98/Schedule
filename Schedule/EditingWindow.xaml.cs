475f98c298ca8c961acd26b49a7dd51ed6937e06
stormkirill98 <stormkirill98@gmail.com>
2019-12-05 20:34:19 +0300

    read schedule table

--- Schedule/EditingWindow.xaml.cs
+++ Schedule/EditingWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DatabaseUtils;
 
 namespace Schedule
 {
@@ -27,23 +28,7 @@ namespace Schedule
         {
             InitializeComponent();
 
-            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Projects\Visual Studio\Schedule\ScheduleBD.accdb";
-
-            OleDbConnection thisConnection = new OleDbConnection(connectionString);
-
-            // Create command for this connection
-            OleDbDataAdapter thisAdapter = new OleDbDataAdapter(
-                             "SELECT ScheduleID, DayOfWeekID FROM Schedule", thisConnection);
-
-            // Create DataSet to contain related data tables, rows, and columns
-            DataSet thisDataSet = new DataSet();
-
-            // Fill DataSet using query defined previously for DataAdapter
-            thisAdapter.Fill(thisDataSet, "Schedule");
-
-            // Show data before change
-            Console.WriteLine("name before change: {0}",
-                 thisDataSet.Tables["Schedule"].Rows[0]);
+            DatabaseUtils.Class1.print();
         }
     }
 }
