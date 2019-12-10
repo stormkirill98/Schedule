7473bd4e84e8405070f25bedb3662c2c00c41b27
stormkirill98 <stormkirill98@gmail.com>
2019-12-11 00:12:41 +0300

    Setup layout

---
+++ Schedule/Lesson.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Schedule
+{
+    public partial class Lesson : UserControl
+    {
+        public static readonly DependencyProperty NumberProperty = DependencyProperty.Register("Number", typeof(int), typeof(Lesson));
+        public int Number
+        {
+            get { return (int)GetValue(NumberProperty); }
+            set { SetValue(NumberProperty, value); }
+        }
+
+        public Lesson()
+        {
+            InitializeComponent();
+
+            Number = 5;
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EmptyCheckbox_Click(object sender, RoutedEventArgs e)
+        {
+            bool isEmpty = EmptyCheckbox.IsChecked == null ? false : (bool)EmptyCheckbox.IsChecked;
+
+            Visibility visibility = isEmpty ? Visibility.Visible : Visibility.Hidden;
+
+            DisciplineInput.Visibility = visibility;
+            DisciplineTypeInput.Visibility = visibility;
+            CabinetInput.Visibility = visibility;
+            TeacherInput.Visibility = visibility;
+        }
+    }
+}
